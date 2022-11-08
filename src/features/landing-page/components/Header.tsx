fe04fb0a3cccea6fd087b035c9d176f9a84e15f6
Sadat Jubayer <sadatjubayer@gmail.com>
2022-11-08 14:10:13 +0600

    add header

--- src/features/landing-page/components/Header.tsx
+++ src/features/landing-page/components/Header.tsx
@@ -1,8 +1,40 @@
 import { Layout } from 'components';
 import React from 'react';
+import tw, { styled } from 'twin.macro';
+import strings from '../constants/strings';
 
 const Header = () => {
-    return <Layout>Header</Layout>;
+    return (
+        <Layout>
+            <Container>
+                <Title>
+                    {strings.headerTitle}
+                    <Version>{strings.version}</Version>
+                </Title>
+                <ContentRight>
+                    <button>{strings.register}</button>
+                    <button>{strings.login}</button>
+                </ContentRight>
+            </Container>
+        </Layout>
+    );
 };
 
 export default Header;
+
+const Container = tw.div`
+flex justify-between items-center
+`;
+
+const Title = tw.h3`
+font-semibold text-lg md:text-xl relative
+`;
+const Version = tw.span`
+font-normal text-xs absolute pl-1`;
+
+const ContentRight = styled.div`
+    ${tw`flex justify-between items-center space-x-2.5`}
+    button {
+        ${tw`font-medium hover:text-dark/80 active:text-dark/50 text-sm md:text-base`}
+    }
+`;
---
+++ src/features/landing-page/constants/strings.ts
@@ -0,0 +1,13 @@
+const strings = {
+    headerTitle: 'Priyo Money',
+    version: 'beta',
+    register: 'Register',
+    login: 'Sign in',
+
+    heroTitle: 'Priyo Money',
+    heroTitle2: 'Spend Anywhere',
+    heroSubtitle: 'Open a full US Bank Account Instant, All Digital, Secured.',
+    heroCTA: 'Get a free account now',
+};
+
+export default strings;
