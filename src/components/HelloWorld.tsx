d84d98a6a5056f28b1e29a95fc8f79bd3872d7f2
Sadat Jubayer <sadatjubayer@gmail.com>
2022-11-08 13:25:22 +0600

    add TailwindCSS & SC

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import tw from 'twin.macro';
 
 const HelloWorld = () => {
-    return <div>HelloWorld</div>;
+    return (
+        <StyledContainer>
+            <Title> HelloWorld</Title>
+        </StyledContainer>
+    );
 };
 
 export default HelloWorld;
+
+const StyledContainer = tw.div`bg-red-500`;
+
+const Title = tw.h2`text-xl text-center py-5 font-bold text-white`;
