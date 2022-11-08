fa4c2fc59b2c4ffa0f2537663313135b2e945287
Sadat Jubayer <sadatjubayer@gmail.com>
2022-11-08 13:35:35 +0600

    add theme colors

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -11,6 +11,6 @@ const HelloWorld = () => {
 
 export default HelloWorld;
 
-const StyledContainer = tw.div`bg-red-500`;
+const StyledContainer = tw.div`bg-primary`;
 
-const Title = tw.h2`text-xl text-center py-5 font-bold text-white`;
+const Title = tw.h2`text-xl text-center py-5 font-bold text-dark`;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
     content: [],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#FFCA26',
+                dark: '#0B2330',
+                gray: '#787578',
+                heroBg: '#0AA99E',
+                darkBg: '#000000',
+                offWhiteBg: '#FAFAFA',
+            },
+        },
     },
     plugins: [],
 };
